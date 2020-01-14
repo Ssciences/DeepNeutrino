cebffa5f186496c5285cb7a94a3c65b7ccc65000
Daniel de Andres Hernandez <deandres@pcae98.ciemat.es>
2020-01-14 10:53:49 +0100

    update ennumerate

--- ennumerate.sh
+++ ennumerate.sh
@@ -1,14 +1,30 @@
 # simple script to ennumerate and change the names of the files
+
+echo insert path; # the path should be a directory containing root files
+read path;
+cd $path;
+echo moving to $path
+ 
+echo insert tipo; 
+read tipo;
+echo the type is ${tipo}.root
+
+echo insert particle; 
+read particle;
+echo the particle is ${particle}.root
+ 
+
 number_of_files=`ls -l *.root | wc`;
 echo there are $number_of_files ROOT files
 x=0 # initialize file number
 y=1 # counter
 
+
 # the programme
 for i in *.root 
 do 
-mv $i muon_reco_$[x].root
-x=$((x+y))#update the variable x
+mv $i ${particle}_${tipo}_$[x].root
+x=$((x+y)) #update the variable x
 done
 
 
